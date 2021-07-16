bcbe0ccc8bb0ecfcc7781ea579775fa3512f2bfd
RevaYoga Pradana <reva.yoga@ovo.id>
2021-07-16 17:46:55 +0700

    [FEATURE B] Initiate the files

---
+++ RevaApp/RevaApp/FeatureB/FeatureBViewController.swift
@@ -0,0 +1,12 @@
+//
+//  FeatureBViewController.swift
+//  RevaApp
+//
+//  Created by Reva Yoga Pradana on 16/07/21.
+//
+
+import UIKit
+
+class FeatureBViewController: UIViewController {
+    
+}
